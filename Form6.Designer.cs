a5567f580b7b90fdfa5ab25db3cf3ee9dc68bfe5
AnTran210 <21520553@gm.uit.edu.vn>
2023-10-30 21:51:19 +0700

    Add project files.

---
+++ Form6.Designer.cs
@@ -0,0 +1,248 @@
+﻿
+namespace QuanLiSoTietKiem
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonQD1 = new System.Windows.Forms.Button();
+            this.textTien = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboLoaiTK = new System.Windows.Forms.ComboBox();
+            this.textLaisuat = new System.Windows.Forms.TextBox();
+            this.textKyhan = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(250, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(291, 38);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Thay đổi quy định";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tiền gửi tối thiểu";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonQD1);
+            this.groupBox1.Controls.Add(this.textTien);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(31, 118);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(712, 89);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "QĐ1";
+            // 
+            // buttonQD1
+            // 
+            this.buttonQD1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonQD1.Location = new System.Drawing.Point(575, 32);
+            this.buttonQD1.Name = "buttonQD1";
+            this.buttonQD1.Size = new System.Drawing.Size(110, 34);
+            this.buttonQD1.TabIndex = 3;
+            this.buttonQD1.Text = "Cập nhật";
+            this.buttonQD1.UseVisualStyleBackColor = true;
+            this.buttonQD1.Click += new System.EventHandler(this.buttonQD1_Click);
+            // 
+            // textTien
+            // 
+            this.textTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textTien.Location = new System.Drawing.Point(145, 36);
+            this.textTien.Name = "textTien";
+            this.textTien.Size = new System.Drawing.Size(232, 27);
+            this.textTien.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboLoaiTK);
+            this.groupBox2.Controls.Add(this.textLaisuat);
+            this.groupBox2.Controls.Add(this.textKyhan);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.buttonUpdate);
+            this.groupBox2.Controls.Add(this.buttonCreate);
+            this.groupBox2.Controls.Add(this.buttonDelete);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(31, 231);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(712, 190);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "QĐ3";
+            // 
+            // comboLoaiTK
+            // 
+            this.comboLoaiTK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboLoaiTK.FormattingEnabled = true;
+            this.comboLoaiTK.Location = new System.Drawing.Point(117, 30);
+            this.comboLoaiTK.Name = "comboLoaiTK";
+            this.comboLoaiTK.Size = new System.Drawing.Size(260, 28);
+            this.comboLoaiTK.TabIndex = 11;
+            // 
+            // textLaisuat
+            // 
+            this.textLaisuat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textLaisuat.Location = new System.Drawing.Point(92, 141);
+            this.textLaisuat.Name = "textLaisuat";
+            this.textLaisuat.Size = new System.Drawing.Size(285, 27);
+            this.textLaisuat.TabIndex = 10;
+            // 
+            // textKyhan
+            // 
+            this.textKyhan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textKyhan.Location = new System.Drawing.Point(153, 88);
+            this.textKyhan.Name = "textKyhan";
+            this.textKyhan.Size = new System.Drawing.Size(224, 27);
+            this.textKyhan.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(17, 144);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Lãi suất";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(17, 91);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(130, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Thời gian kỳ hạn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(17, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Loại kỳ hạn";
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonUpdate.Location = new System.Drawing.Point(575, 137);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(110, 34);
+            this.buttonUpdate.TabIndex = 6;
+            this.buttonUpdate.Text = "Sửa";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCreate.Location = new System.Drawing.Point(575, 26);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(110, 34);
+            this.buttonCreate.TabIndex = 5;
+            this.buttonCreate.Text = "Thêm";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDelete.Location = new System.Drawing.Point(575, 84);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(110, 34);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "Xóa";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form6
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form6";
+            this.Text = "Thay đổi quy định";
+            this.Load += new System.EventHandler(this.Form6_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textTien;
+        private System.Windows.Forms.Button buttonQD1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.ComboBox comboLoaiTK;
+        private System.Windows.Forms.TextBox textLaisuat;
+        private System.Windows.Forms.TextBox textKyhan;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ Form6.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace QuanLiSoTietKiem
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection connection;
+        string connectStr = @"Data Source=LAPTOP-9VMFMRRH\SQLEXPRESS;Initial Catalog=QLSTK;Integrated Security=True";
+
+        private void Form6_Load(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(connectStr);
+            connection.Open();
+            SqlCommand command;
+            command = connection.CreateCommand();
+            command.CommandText = "select LOAITK from LOAITIETKIEM";
+            SqlDataAdapter adapter;
+            adapter = new SqlDataAdapter();
+            adapter.SelectCommand = command;
+            DataTable table = new DataTable();
+            table.Clear();
+            adapter.Fill(table);
+            foreach (DataRow row in table.Rows)
+            {
+                comboLoaiTK.Items.Add(row[0]);
+            }
+            connection.Close();
+        }
+
+        private void buttonQD1_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(connectStr);
+            connection.Open();
+            SqlCommand command;
+            command = connection.CreateCommand();
+            command.CommandText = "update HANMUC set TIENTOITHIEU = "+textTien.Text+" where MYKEY = 1";
+            int flag = command.ExecuteNonQuery();
+            if (flag == 1)
+            {
+                MessageBox.Show("Cập nhật hạn mức thành công.");
+            }
+            connection.Close();
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(connectStr);
+            connection.Open();
+            SqlCommand command;
+            command = connection.CreateCommand();
+            command.CommandText = "insert into LOAITIETKIEM values('" + comboLoaiTK.Text + "'," + textKyhan.Text + "," + textLaisuat.Text + ")";
+            int flag = command.ExecuteNonQuery();
+            if (flag == 1)
+            {
+                MessageBox.Show("Thêm loại kỳ hạn thành công.");
+            }
+            connection.Close();
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(connectStr);
+            connection.Open();
+            SqlCommand command;
+            command = connection.CreateCommand();
+            command.CommandText = "delete from LOAITIETKIEM where LOAITK = N'" + comboLoaiTK.Text + "'";
+            int flag = command.ExecuteNonQuery();
+            if (flag == 1)
+            {
+                MessageBox.Show("Xóa loại kỳ hạn thành công.");
+            }
+            connection.Close();
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(connectStr);
+            connection.Open();
+            SqlCommand command;
+            command = connection.CreateCommand();
+            command.CommandText = "update LOAITIETKIEM set LAISUAT = " + textLaisuat.Text + " where LOAITK = '" + comboLoaiTK.Text + "'";
+            int flag = command.ExecuteNonQuery();
+            if (flag == 1)
+            {
+                MessageBox.Show("Cập nhật lãi suất thành công.");
+            }
+            connection.Close();
+        }
+    }
+}
